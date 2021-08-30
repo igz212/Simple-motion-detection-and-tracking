e9993547bba9c369a01ab9fc0e71375834043105
Iman GhashghaeiZadeh <44636790+igz212@users.noreply.github.com>
2021-08-31 00:36:42 +0430

    Update simple_motion_detection.py

--- simple_motion_detection.py
+++ simple_motion_detection.py
@@ -1,6 +1,10 @@
 import cv2
 
 captured = cv2.VideoCapture(0)
+'''
+Eng. : cv2.VideoCapture(0) would be capture from your camera, to change it as a file you can pass address to the function
+De.  : cv2.VideoCapture(0) würde von Ihrer Kamera aufgenommen werden, um es als gespeichert Datei zu ändern, können Sie die Adresse an die Funktion übergeben
+'''
 
 _, frame1 = captured.read()
 _, frame2 = captured.read()
@@ -9,13 +13,26 @@ assert captured.isOpened(), f"Something went wrong. video could not be captured.
 
 while captured.isOpened():
     difference = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)  
-    blur = cv2.GaussianBlur(gray, (7, 7), 0)   
+    gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray', gray)
+
+    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+    cv2.imshow('blur', blur)
+
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     cv2.imshow('threshold', thresh)
+    
     dilated = cv2.dilate(thresh, None, iterations=3)
     cv2.imshow('dilated', dilated)
+    
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    '''
+    Eng. : fun to see what happened in the background with drawing contours :)
+    De.  : Es macht Spaß zu sehen, was im Hintergrund mit dem Zeichnen von Konturen passiert ist :) 
+    '''
+     
+    # cv2.drawContours(frame1, contours, -1, (25, 255, 25), 2)
+
     minimum_area_to_detect_motion = 650
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
@@ -28,6 +45,7 @@ while captured.isOpened():
     cv2.imshow('feed', frame1)
     frame1 = frame2
     ret, frame2 = captured.read()
+
     #  stop key would be 's'
     if cv2.waitKey(40) & 0xFF == ord('s'):
         break
